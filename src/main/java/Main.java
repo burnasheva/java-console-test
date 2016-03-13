4388e8cd842d188d8c55bff2b2d00d40b4302c98
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-03-13 23:19:52 +0300

    add environmental variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,10 +10,12 @@ public class Main {
         System.err.println("123456");
         System.err.println("http://user:123456@jetbrains.com");
 
-        System.err.println("env.pwd" + System.getenv("pwd"));
-        System.err.println("env pwd_url"+System.getenv("pwd_url"));
+        if (System.getenv("pwd") != null && System.getenv("pwd_url") != null) {
+            System.err.println("env.pwd" + System.getenv("pwd"));
+            System.err.println("env pwd_url"+System.getenv("pwd_url"));
+        }
 
-        if (args != null & args[0] != null) {
+        if (args != null && args[0] != null) {
             System.err.println(args[0]);
         }
     }
